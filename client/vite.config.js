d3943c50a421d16ecf5878ee9b9924b593b05c4e
Coltahhh <harrisonphillips25@gmail.com>
2025-02-27 12:55:47 -0600

    added dependencies

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,13 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
-    root: './',       // Explicitly set the root directory
     build: {
-        outDir: 'dist', // Ensure this matches your build output
-        rollupOptions: {
-            input: {
-                main: './index.html', // Explicit path to index.html
-            },
-        },
+        outDir: 'dist', // Ensure this matches Vercel's Output Directory
     },
 });
