5ca74ce09e875e9d7562a0031dbe1eb5dbf3613e
Coltahhh <harrisonphillips25@gmail.com>
2025-02-27 12:30:36 -0600

    added dependencies

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
-    server: {
-        port: 3000,
-        open: true
-    }
-})
+    root: './',       // Explicitly set the root directory
+    build: {
+        outDir: 'dist', // Ensure this matches your build output
+        rollupOptions: {
+            input: {
+                main: './index.html', // Explicit path to index.html
+            },
+        },
+    },
+});
