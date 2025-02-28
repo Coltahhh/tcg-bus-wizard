06dfe9c92527e6b80e88025c19364f3ffbeaf1e3
Coltahhh <harrisonphillips25@gmail.com>
2025-02-27 23:53:11 -0600

    added dependencies

--- client/src/firebase/firebaseConfig.js
+++ client/src/firebase/firebaseConfig.js
@@ -1,15 +1,22 @@
-// client/src/firebase/firebaseConfig.js
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID
+    apiKey: "AIzaSyAbadT_JnHGA3PV-HljH4_MMG97rZr8FP8",
+    authDomain: "tcg-bus-wizard.firebaseapp.com",
+    projectId: "tcg-bus-wizard",
+    storageBucket: "tcg-bus-wizard.firebasestorage.app",
+    messagingSenderId: "955883115244",
+    appId: "1:955883115244:web:485e9640ffbf0da2078bc9",
+    measurementId: "G-21GBTLZLBX"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
