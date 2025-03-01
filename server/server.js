62a58031ca075b37bfe48cda0e3d29ccfedb84cb
Coltahhh <harrisonphillips25@gmail.com>
2025-02-28 21:04:58 -0600

    added dependencies

--- server/server.js
+++ server/server.js
@@ -6,11 +6,22 @@ const admin = require('firebase-admin');
 const { initializeFirestore } = require('firebase-admin/firestore');
 
 // Initialize Firebase Admin
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+const serviceAccount = {
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT
+};
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: process.env.FIREBASE_DATABASE_URL
+    databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`
 });
 
 // Initialize Firestore
