84dbebfc45265edcee32bf2e8ecaa3e1c973658f
Coltahhh <harrisonphillips25@gmail.com>
2025-03-02 14:54:23 -0600

    "Add profile page with editing functionality"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,8 @@
-// client/src/App.jsx
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/Auth/Login';
-import SignUp from './components/Auth/SignUp';
-import Home from './components/Home';
-import Navigation from './components/Navigation';
-import Profile from './components/Profile/Profile';
-
-function App() {
+export default function App() {
     return (
-        <Router>
-            <Navigation />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<SignUp />} />
-                <Route
-                    path="/profile"
-                    element={
-                        <ProtectedRoute>
-                            <Profile />
-                        </ProtectedRoute>
-                    }
-                />
-                        </Routes>
-        </Router>
+        <div style={{ backgroundColor: 'black', color: 'white', padding: '2rem' }}>
+            <h1>TCGBusWizard is Working!</h1>
+            <p>Base React component rendered successfully</p>
+        </div>
     );
 }
-
-export default App;
