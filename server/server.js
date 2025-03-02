1437322afa5c3de68dafb5eace9e141b52f2b688
Coltahhh <harrisonphillips25@gmail.com>
2025-03-02 03:45:37 -0600

    added dependencies

--- server/server.js
+++ server/server.js
@@ -6,16 +6,17 @@ const { initializeFirestore } = require('firebase-admin/firestore');
 
 // Initialize Firebase Admin
 const serviceAccount = {
-    type: process.env.FIREBASE_TYPE,
-    project_id: process.env.FIREBASE_PROJECT_ID,
-    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    client_id: process.env.FIREBASE_CLIENT_ID,
-    auth_uri: process.env.FIREBASE_AUTH_URI,
-    token_uri: process.env.FIREBASE_TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER,
-    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT
+    type: "service_account",
+    project_id: "tcg-bus-wizard",
+    private_key_id: "4b7add9508717e156ccda8196c1cbf247a74b934",
+    private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCnwtspP56L2DOw\nPM1xsyDXk31Tetp3JS9kNeS2pyUknprJUk+GwMg4Ys780uIpOjg5cupSnIoP13Lx\n21UgrNui0bLXPVpf/PzgBAIlUAxyRgSPF6jxBRKwBb352tU49fEmJBboTVk9TcWD\nOKJurx+hobMY0i3fGHE+AjWPTXxyrZuaWS47SYgwM7PuvlmEu2LhEC3h/z9CJp7W\npWZ1dOhKWlIXadyDccLw09oMXyEGUfE6T6PBulJHkHekMbtt0Sji9RucKT/jGbS1\nWxmx2pwS8efGVygniml0l/8zXU6C/h8ohC5wBNCcsCu31kVEsUaVcQNfGX3TspWL\nx95fZ6RjAgMBAAECggEAG3XOWPykb6CmqVDykgtvs0ndqFYCP6FzFfSqaOmIL2vU\ny2JxSQ3yWCR4XDMKuRPiEylHyNj90mUCWCt+TjhtRUQNAaV1zyOCowqU7VWii+wB\nIAkP6s/ZRL3T4HudDlNXs1YR99l9Y4cA7x9Mm1CIHXVx8vyobI28L5m9+aSF2+Fh\n6djDY/t+d8i/AEPY8G15L/hQcjBVsprKII+I1Oo0ZXk575+AY+ZruIKxSGgRwdW9\nsCU4KT76f8K8WfgtsTkReL48jP+zkTV6r0mobn7gYrRxH4VtrGoLJx5UG4RG+3CI\nwjoaDJR9kB6t/1/5asC7sKE8tXzqmQwU0pIRnJhETQKBgQDixbKabMkPh9JfYjZp\nqiLaOw6/tGKCQH98V/6/vgRiMGkBcknIhtBoSndSb4qSfcKLjxljWJLpwPXEdmXR\n9jOEWYpqYrZAW62gopMIXOWyvBQVA+wsb9xTOHnDqCRt5pW/klazYGYF5O0kZP+y\nJY5sqMYAek+uv3hpZM193F+LbQKBgQC9YhmJOqTUQ72zXCaLITYRjphV9MwrpyMs\nfD+xaKbqBvS6zqNXVUb6hxcB11YP8A7Ub5D5j0Xvsnj0JLo/s6m/vwKXR1sYzZGq\npfEn9o5AduhsRtx/y8e6HcaNHUWh9oqQzLjQwH+iVmQtwU4K12XNhL4SCrqbBVHo\ngaO8GPu9DwKBgDeNxQZNtifG10OktL3H/0fFBgTytNtB37lfeToR29MOFvtSuz6T\nwER8UaTxTFo80scLpGWRElSPf+XEN4drMA2xPdZ4Hju/+dYLXcfDskXKC4Vmc5oj\nQS9l+3ZyH5KI1ZCb0f+ZkwTIo0uyLm2/A5gc9HyzegfOQjpXhnvZ29I1AoGAHEdb\nvMwO9MswtmmaL8hDxJdgU+V730DT1b6v3BPeaK+lN4fHiDDpqlslqla3OSYFcN8/\n7KPiM4qY2Qvq67wuKr2g62nyRl/b0nl46xi74RBgYFqsbkDFkBsGHOg0D506z6hi\nKHm2FU2FI/fraV8Eio25vAPyHVfMBUXmZx3r6S0CgYABd1u5a6szCuPkwnD76PZz\n1ZxwRO/ZE/YIV+SZx2kkh+RJHDXOKy/+hEVkmy+O3GQpOVgYR7+ZwicERUKca06U\nW5Wj9l/tjX6zpuBf3V1qXq8rkMHsrJP7nOR16aaxOv6rcYTJ/KfIYi8L8XyNaRu0\npwKbi7nYYhJAbAXDcn/JWw==\n-----END PRIVATE KEY-----\n",
+    client_email: "firebase-adminsdk-fbsvc@tcg-bus-wizard.iam.gserviceaccount.com",
+    client_id: "116671003595803542780",
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40tcg-bus-wizard.iam.gserviceaccount.com",
+
 };
 
 admin.initializeApp({
